e26a9d210bd942421d79bc5d5c233da6aad36a21
Mohammad Reza esmailzadeh <smr8012101067@gmail.com>
2023-01-22 16:00:07 +0330

    hh

---
+++ Tailwind/session 16/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {
+      spacing: {
+        mohammad: "20px",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+  },
+  plugins: [],
+};
