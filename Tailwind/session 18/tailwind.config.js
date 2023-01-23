d89a316aa2992d23ec9fac9f8f6845f833c91dc6
Mohammad Reza esmailzadeh <smr8012101067@gmail.com>
2023-01-23 15:54:04 +0330

    mmm

--- Tailwind/session 18/tailwind.config.js
+++ Tailwind/session 18/tailwind.config.js
@@ -2,21 +2,6 @@
 module.exports = {
   content: ["./public/**/*.html"],
   theme: {
-    colors: {
-      primaryColor: "#0891b2",
-      secondaryColor: "#0e7490",
-      mohammad: {
-        100: "#cffafe",
-        200: "#a5f3fc",
-        300: "#67e8f9",
-        400: "#22d3ee",
-        500: "#06b6d4",
-        600: "#0891b2",
-        700: "#0e7490",
-        800: "#155e75",
-        900: "#164e63",
-      },
-    },
     extend: {
       spacing: {
         mohammad: "20px",
